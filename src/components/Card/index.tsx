8fc4463c0e3ae97a986b4fd3fc4de768012ad941
Maycon <mayconrr13@gmail.com>
2022-01-25 21:43:18 -0300

    feat: added apollo client and configured it

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,32 @@
-const Card = () => {
+import { MissionProps } from "./types"
+
+import defaultMissionImage from "/images/default-mission-image.jpg"
+
+type CardProps = {
+    props: MissionProps
+}
+
+const Card = ({props}: CardProps) => {
+    console.log(props.links)
     return (
-        <div>
-            This is a card!
-        </div>
+        <article>
+            <header>
+                <div>
+                    {
+                        props.links.flickr_images.length === 0 ? (
+                            <img src={defaultMissionImage} alt={props.mission_name} width={320} height={180}/>
+                        ) : (
+                            <img src={props.links.flickr_images[0]} alt={props.mission_name} width={320} height={180} />
+                        )
+                    }
+
+
+                    <h3>{props.mission_name}</h3>
+                    <span>{props.launch_date_local}</span>
+                </div>
+            </header>
+            {props.mission_name}
+        </article>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import { ApolloProvider } from "@apollo/client";
+import client from './services/apolloClient';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
